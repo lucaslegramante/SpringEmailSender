6c69b6edee87681b5794b332e341c6016573ea06
lucaslegramante <lucaslegramante@gmail.com>
2025-03-25 00:28:39 -0300

    feat: adjust crenditals

--- src/main/kotlin/org/example/springemailsender/infra/AwsSesConfig.kt
+++ src/main/kotlin/org/example/springemailsender/infra/AwsSesConfig.kt
@@ -1,14 +1,29 @@
 package org.example.springemailsender.infra
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class AwsSesConfig {
+class AwsSesConfig(
+    @Value("\${aws.region}")
+    private val region: String,
+    @Value("\${aws.accessKey}")
+    private val accessKey: String,
+    @Value("\${aws.secretKey}")
+    private val secretKey: String
+) {
+
     @Bean
     fun amazonSimpleEmailService(): AmazonSimpleEmailService {
-        return AmazonSimpleEmailServiceClientBuilder.standard().build()
+        val awsCredentials = BasicAWSCredentials(accessKey, secretKey)
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+            .build()
     }
 }
