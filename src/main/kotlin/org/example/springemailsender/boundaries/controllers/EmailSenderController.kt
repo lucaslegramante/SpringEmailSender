5c043539119e3008f23d5cea74c44e931bc0d1f1
lucaslegramante <lucaslegramante@gmail.com>
2025-03-24 23:59:30 -0300

    feat: create post mapping on controller

--- src/main/kotlin/org/example/springemailsender/boundaries/controllers/EmailSenderController.kt
+++ src/main/kotlin/org/example/springemailsender/boundaries/controllers/EmailSenderController.kt
@@ -1,4 +1,31 @@
 package org.example.springemailsender.boundaries.controllers
 
-class EmailSenderController {
+import org.example.springemailsender.core.application.EmailRequest
+import org.example.springemailsender.core.application.EmailSenderUseCase
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/api/email")
+class EmailSenderController(
+    private val emailSenderUseCase: EmailSenderUseCase,
+) {
+
+    @PostMapping("/send")
+    fun sendEmail(
+        @RequestBody emailRequest: EmailRequest,
+    ): ResponseEntity<String> {
+        try {
+            emailSenderUseCase.sendEmail(
+                emailRequest
+            )
+            return ResponseEntity.ok("Email sent successfully")
+        } catch (e: EmailServiceException) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error sending email")
+        }
+    }
 }
